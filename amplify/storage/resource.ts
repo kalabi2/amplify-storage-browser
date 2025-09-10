3d79fd7472a7539637617550da43c39aab643742
kalabi <kalabi@deloitte.ie>
2025-09-10 14:21:15 +0100

    adding existing bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'myStorageBucket',
+  name: 'dedicated-switch-bucket ',
   isDefault: true,
    access: (allow) => ({
     'switch-folder/*': [
